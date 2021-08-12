b8205be4865560e1ed8f934ac9cd073666c6ba04
MLEG <jianjunh@cse.sc.edu>
2021-08-12 18:20:15 -0400

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -1,15 +1,6 @@
-
-str1=['D:\lib\site-packages\pymoo\\algorithms\\nsga2.py',
-'D:\lib\site-packages\pymoo\\algorithms\genetic_algorithm.py',
-'D:\lib\site-packages\pymoo\model\mating.py',
-'D:\lib\site-packages\pymoo\model\infill.py']
-
-str2=['nsga2.py','genetic_algorithm.py','mating.py','infill.py']
-for i in range(len(str1)):
-    fp = open(str1[i], "w")
-    fp.truncate()
-    with open(str2[i], "r") as fileobj:
-        text = fileobj.read()
-    with open(str1[i], "w") as fw:
-        fw.write(text)
+export PYMOODIR=$1
+cp nsga2.py ${PYMOODIR}\algorithms\nsga2.py
+cp genetic_algorithm.py ${PYMOODIR}\algorithms\genetic_algorithm.py
+cp mating.py ${PYMOODIR}\model\mating.py
+cp infill.py ${PYMOODIR}\model\infill.py
 
